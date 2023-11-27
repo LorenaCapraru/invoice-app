8298149ffb16f74d03f3b69589544bcba2495305
Lorena <lorenacapraru@gmail.com>
2023-11-27 14:39:37 +0000

    Static Design ready for Sidebar

--- src/app/components/Sidebar/Sidebar.tsx
+++ src/app/components/Sidebar/Sidebar.tsx
@@ -4,7 +4,7 @@ import Image from "../../../../node_modules/next/image";
 const Sidebar = () => {
   return (
     <div className="sidebar-main">
-      <div className="icon-name-wrap">
+      <div className="icon-name-wrap-overview">
         <Image
           src="/icons/bars.svg"
           alt="close navbar"
@@ -13,10 +13,33 @@ const Sidebar = () => {
         />
         <p className="overview-text">Overview</p>
       </div>
-
-      <p>Invoices</p>
-      <p>Clients</p>
-      <p>Employees</p>
+      <div className="icon-name-wrap">
+        <Image
+          src="/icons/invoice.svg"
+          alt="close navbar"
+          width={25}
+          height={25}
+        />
+        <p>Invoices</p>
+      </div>
+      <div className="icon-name-wrap">
+        <Image
+          src="/icons/clients.svg"
+          alt="close navbar"
+          width={25}
+          height={25}
+        />
+        <p>Clients</p>
+      </div>
+      <div className="icon-name-wrap">
+        <Image
+          src="/icons/employees.svg"
+          alt="close navbar"
+          width={25}
+          height={25}
+        />
+        <p>Employees</p>
+      </div>
     </div>
   );
 };
