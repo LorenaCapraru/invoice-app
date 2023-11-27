b52768f48e47a3a2582d5fe29090d3765168ea1a
Lorena <lorenacapraru@gmail.com>
2023-11-27 14:12:25 +0000

    Sidebar structure partially done

--- src/app/components/Sidebar/Sidebar.tsx
+++ src/app/components/Sidebar/Sidebar.tsx
@@ -1,7 +1,22 @@
+import "./Sidebar.css";
+import Image from "../../../../node_modules/next/image";
+
 const Sidebar = () => {
   return (
     <div className="sidebar-main">
-      <p>Overview</p>
+      <div className="icon-name-wrap">
+        <Image
+          src="/icons/bars.svg"
+          alt="close navbar"
+          width={25}
+          height={25}
+        />
+        <p className="overview-text">Overview</p>
+      </div>
+
+      <p>Invoices</p>
+      <p>Clients</p>
+      <p>Employees</p>
     </div>
   );
 };
