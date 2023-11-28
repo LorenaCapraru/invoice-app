8c4fbe59eb76cb3ae465fb946145d6e545040afd
Lorena <lorenacapraru@gmail.com>
2023-11-28 20:07:49 +0000

    Header done

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -5,22 +5,29 @@ import Image from "../../../../node_modules/next/image";
 import { useState } from "react";
 
 const Header = () => {
-  const [isMenuClick, setIsMenuClick] = useState<boolean>(false);
-  const handleMenuClick = () => {
-    setIsMenuClick(!isMenuClick);
-  };
-
   return (
-    <div className="header-main">
-      {isMenuClick && <Sidebar />}
-      <Image
-        src="/icons/menu.svg"
-        alt="close navbar"
-        width={30}
-        height={30}
-        onClick={handleMenuClick}
-      />
-    </div>
+    <>
+      <Sidebar />
+      <div className="header-main">
+        <Image
+          src="/icons/search.svg"
+          alt="user icon"
+          width={20}
+          height={20}
+          className="search-icon"
+        />
+        <div className="search-user">
+          Hi, Gabriel
+          <Image
+            src="/images/user1.png"
+            alt="user icon"
+            width={30}
+            height={30}
+            className="user-icon"
+          />
+        </div>
+      </div>
+    </>
   );
 };
 
