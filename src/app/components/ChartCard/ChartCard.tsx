2e8134197253bcbe7b44907602fb8146713134f1
Lorena <lorenacapraru@gmail.com>
2023-11-29 12:47:19 +0000

    create chart component

---
+++ src/app/components/ChartCard/ChartCard.tsx
@@ -0,0 +1,12 @@
+"use client";
+import "./ChartCard.css";
+import { useState, useEffect } from "react";
+
+const ChartCard = () => {
+  //   return <div className="chart-main">chaaaart</div>;
+
+
+  return <canvas id="percentageChart" width={400} height={400} />;
+};
+
+export default ChartCard;
