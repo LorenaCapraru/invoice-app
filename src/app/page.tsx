14369716d3bd83bfeadc1f261bec9f850a6f87b1
Lorena <lorenacapraru@gmail.com>
2023-11-29 12:47:42 +0000

    added main component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header/Header";
 import Sidebar from "./components/Sidebar/Sidebar";
+import Main from "./components/Main/Main";
 export default function Home() {
   return (
     <main>
       <Header />
+      <Main />
     </main>
   );
 }
