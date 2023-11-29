3a94d8741682a4283bfe59686b8aa3661f52b34c
Lorena <lorenacapraru@gmail.com>
2023-11-29 13:31:38 +0000

    Pie Chart added

--- src/app/components/ChartCard/ChartCard.tsx
+++ src/app/components/ChartCard/ChartCard.tsx
@@ -3,10 +3,11 @@ import "./ChartCard.css";
 import { useState, useEffect } from "react";
 
 const ChartCard = () => {
-  //   return <div className="chart-main">chaaaart</div>;
-
-
-  return <canvas id="percentageChart" width={400} height={400} />;
+  return (
+    <div className="chart-main">
+      <canvas id="percentageChart" width={400} height={400} />
+    </div>
+  );
 };
 
 export default ChartCard;
--- src/app/components/Main/Main.tsx
+++ src/app/components/Main/Main.tsx
@@ -1,7 +1,6 @@
 "use client";
 import "./Main.css";
 import invoicesData from "./invoices.json";
-import ChartCard from "../ChartCard/ChartCard";
 import React, { useEffect, useState } from "react";
 import Chart from "chart.js";
 
@@ -14,38 +13,6 @@ const Main = () => {
   const [paid, setPaid] = useState<Invoice[]>([]);
   const [send, setSend] = useState<Invoice[]>([]);
   const [pending, setPending] = useState<Invoice[]>([]);
-
-  useEffect(() => {
-    const filterPaid = [...invoicesData.invoices].filter(
-      (el) => el.status === "paid"
-    );
-    const filterSend = [...invoicesData.invoices].filter(
-      (el) => el.status === "send"
-    );
-    const filterStatus = [...invoicesData.invoices].filter(
-      (el) => el.status === "status"
-    );
-    setPaid(filterPaid);
-    setSend(filterPaid);
-    setPending(filterPaid);
-
-    const totalInvoices = invoicesData.invoices.length;
-    const paidPercentage = (paid.length / totalInvoices) * 100;
-    const sendPercentage = (send.length / totalInvoices) * 100;
-    const pendingPercentage = (pending.length / totalInvoices) * 100;
-
-    // Update chart data
-    setChartData({
-      labels: ["Paid", "Send", "Pending"],
-      datasets: [
-        {
-          data: [paidPercentage, sendPercentage, pendingPercentage],
-          backgroundColor: ["#36A2EB", "#FFCE56", "#FF6384"],
-        },
-      ],
-    });
-  }, []);
-
   const [chartData, setChartData] = useState<Chart.ChartData>({
     labels: ["Paid", "Send", "Pending"],
     datasets: [
@@ -55,6 +22,56 @@ const Main = () => {
       },
     ],
   });
+
+  useEffect(() => {
+    const filterPaid = invoicesData.invoices.filter(
+      (el) => el.status === "paid"
+    );
+    const filterSend = invoicesData.invoices.filter(
+      (el) => el.status === "send"
+    );
+    const filterPending = invoicesData.invoices.filter(
+      (el) => el.status === "pending"
+    );
+
+    setPaid(filterPaid);
+    setSend(filterSend);
+    setPending(filterPending);
+
+    const totalInvoices = invoicesData.invoices.length;
+    const paidPercentage = (filterPaid.length / totalInvoices) * 100;
+    const sendPercentage = (filterSend.length / totalInvoices) * 100;
+    const pendingPercentage = (filterPending.length / totalInvoices) * 100;
+
+    const ctx = document.getElementById("percentageChart") as HTMLCanvasElement;
+
+    //first  gradient
+    const gradient = ctx.getContext("2d")!.createLinearGradient(0, 0, 400, 0);
+    gradient.addColorStop(0, "#1fdad6"); // start color
+    gradient.addColorStop(0.5, "#29e4cf"); // middle color
+    gradient.addColorStop(1, "#33edc9"); // end color
+
+    //second  gradient
+    const gradient2 = ctx.getContext("2d")!.createLinearGradient(0, 0, 400, 0);
+    gradient2.addColorStop(0, "#1cacfa"); // start color
+    gradient2.addColorStop(0.5, "#11c9fb"); // middle color
+    gradient2.addColorStop(1, "#0ccbfc"); // end color
+
+    //third  gradient
+    const gradient3 = ctx.getContext("2d")!.createLinearGradient(0, 0, 400, 0);
+    gradient3.addColorStop(0, "#ffab00"); // start color
+    gradient3.addColorStop(0.5, "#ffc73a"); // middle color
+    gradient3.addColorStop(1, "#ffe27a"); // end color
+    setChartData({
+      labels: ["Paid", "Send", "Pending"],
+      datasets: [
+        {
+          data: [paidPercentage, sendPercentage, pendingPercentage],
+          backgroundColor: [gradient, gradient2, gradient3],
+        },
+      ],
+    });
+  }, []);
 
   useEffect(() => {
     // Create chart
@@ -71,11 +88,10 @@ const Main = () => {
     <div className="main-section">
       <h1>Invoices</h1>
       <div className="main-body">
-        {/* {invoicesData.invoices.map((el, index) => (
-          <div key={index}>{el.status}</div>
-        ))} */}
-        <ChartCard />
-        <canvas id="percentageChart" width={400} height={400} />;
+        <div className="chart">
+          <p className="total-invoices">{invoicesData.invoices.length}</p>
+          <canvas id="percentageChart" width={20} height={20} />
+        </div>
       </div>
     </div>
   );
