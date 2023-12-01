4f50b3002a0d392919f843dcda1a4440ef9becaa
Lorena <lorenacapraru@gmail.com>
2023-12-01 14:32:29 +0000

    style and add calendar for start/end

--- src/app/invoice/page.tsx
+++ src/app/invoice/page.tsx
@@ -1,8 +1,30 @@
+"use client";
 import Sidebar from "../components/Sidebar/Sidebar";
 import "./page.css";
 import Image from "../../../node_modules/next/image";
+import DateSelector from "./components/DateSelector/DateSelector";
+import { useState } from "react";
 
 const Invoice = () => {
+  const [startWeekClick, seStartWeekClick] = useState<boolean | null>(false);
+  const [endWeekClick, seEndWeekClick] = useState<boolean | null>(false);
+
+  const [weekStart, setWeekStart] = useState<Date | null>(null);
+  const [weekEnd, setWeekEnd] = useState<Date | null>(null);
+
+  const handleDateChangeStart = (date: Date | null) => {
+    setWeekStart(date);
+  };
+  const handleDateChangeEnd = (date: Date | null) => {
+    setWeekEnd(date);
+  };
+  const handleStartWeekClick = () => {
+    seStartWeekClick(!startWeekClick);
+  };
+  const handleEndWeekClick = () => {
+    seEndWeekClick(!endWeekClick);
+  };
+
   return (
     <div>
       <Sidebar />
@@ -24,7 +46,45 @@ const Invoice = () => {
           height={20}
         />
       </div>
-      <div className="invoice-date">date</div>
+      <div className="invoice-date">
+        <div className="week-start">
+          <div className="calendar-icon">
+            <Image
+              src="./icons/calendar.svg"
+              alt="calendar"
+              width={20}
+              height={20}
+              onClick={handleStartWeekClick}
+            />
+            <p>Week Start</p>
+          </div>
+          {startWeekClick && (
+            <DateSelector
+              weekStart={weekStart}
+              handleDateChange={handleDateChangeStart}
+            />
+          )}
+        </div>
+        <div className="week-end">
+          <div className="calendar-icon">
+            <Image
+              src="./icons/calendar.svg"
+              alt="calendar"
+              width={20}
+              height={20}
+              onClick={handleEndWeekClick}
+            />
+            <p>Week End</p>
+          </div>
+          {endWeekClick && (
+            <DateSelector
+              weekStart={weekEnd}
+              handleDateChange={handleDateChangeEnd}
+            />
+          )}
+        </div>
+      </div>
+
       <div className="invoice-body">invoiceee</div>
     </div>
   );
