78b971982074bd6735c1243641714e0a8c2b665c
Lorena <lorenacapraru@gmail.com>
2023-12-01 12:47:15 +0000

    style the invoice page for title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Sidebar from "./components/Sidebar/Sidebar";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
