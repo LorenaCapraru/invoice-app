345307911c663dc766936b6a3eb08e99abf62819
Lorena <lorenacapraru@gmail.com>
2023-12-02 14:08:18 +0000

    get search value and store in search

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,21 +1,24 @@
 "use client";
 import "./Header.css";
 import Image from "../../../../node_modules/next/image";
-import { useState } from "react";
+import { useState, ChangeEvent } from "react";
 
 const Header = () => {
+  const [search, setSearch] = useState<string | undefined>("");
+
+  const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
   return (
     <div className="header-wrapper">
       <div className="header-main">
-        {/* <Image
-          src="/icons/search.svg"
-          alt="user icon"
-          width={20}
-          height={20}
-          className="search-icon"
-        /> */}
         <form action="" className="search-bar">
-          <input type="search" required />
+          <input
+            type="search"
+            required
+            value={search}
+            onChange={handleSearch}
+          />
           <Image
             src="/icons/search.svg"
             alt="user icon"
