feaf31200b90e63f8b780caaa86c368b824f6685
Lorena <lorenacapraru@gmail.com>
2023-12-12 16:04:22 +0000

    Created Employee page

---
+++ src/app/employees/page.tsx
@@ -0,0 +1,13 @@
+import Sidebar from "../components/Sidebar/Sidebar";
+import "./page.css";
+
+const Employees = () => {
+  return (
+    <div className="employees-main">
+      <Sidebar />
+      <div>employees</div>
+    </div>
+  );
+};
+
+export default Employees;
