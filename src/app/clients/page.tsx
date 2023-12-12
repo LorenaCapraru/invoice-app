56f14e61d39e842e386ee44534cfbb7b1072892b
Lorena <lorenacapraru@gmail.com>
2023-12-12 16:01:39 +0000

    Created Clients page

---
+++ src/app/clients/page.tsx
@@ -0,0 +1,13 @@
+import Sidebar from "../components/Sidebar/Sidebar";
+import "./page.css";
+
+const Clients = () => {
+  return (
+    <div className="clients-main">
+      <Sidebar />
+      <div>clients</div>
+    </div>
+  );
+};
+
+export default Clients;
