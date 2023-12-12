00cc2e018df1d967eab61f3cc620a3431e6cc8ce
Lorena <lorenacapraru@gmail.com>
2023-12-12 20:09:24 +0000

    clients cards design

--- src/app/clients/Main/Main.tsx
+++ src/app/clients/Main/Main.tsx
@@ -3,16 +3,19 @@ import Card from "./components/Card/Card";
 import "./Main.css";
 import { useRecoilState } from "recoil";
 import { clientsState } from "@/app/recoil/atoms";
+import CompanyCard from "@/app/invoice/components/CompanyCard/CompanyCard";
 
 const Main = () => {
   const [client, setClient] = useRecoilState(clientsState);
-  console.log("lala", client.clients);
+  console.log("lala", client);
   return (
-    <div>
-      {}
-      <Card />
-      <Card />
-      <Card />
+    <div className="client-main">
+      <p className="clients-header">Clients</p>
+      <div className="cards-main">
+        {client.map((el, index) => (
+          <Card el={el} key={index} />
+        ))}
+      </div>
     </div>
   );
 };
--- src/app/clients/Main/components/Card/Card.tsx
+++ src/app/clients/Main/components/Card/Card.tsx
@@ -1,7 +1,25 @@
 import "./Card.css";
+type Client = {
+  id: number;
+  name: string;
+  address: string;
+};
 
-const Card = () => {
-  return <div>card</div>;
+const Card = ({ el }: { el: Client }) => {
+  const takeInitials = (input: string): string => {
+    const initials = input.split(" ");
+    return initials[0][0] + (initials[1] ? initials[1][0] : "");
+  };
+
+  return (
+    <div className="card-main">
+      <p className="initials">{takeInitials(el.name)}</p>
+      <div>
+        <div className="card-name">{el.name}</div>
+        <div className="card-address">{el.address}</div>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
