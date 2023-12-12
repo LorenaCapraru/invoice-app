7726a0be0a15a45e08e1b1740c5900c441c7a8e9
Lorena <lorenacapraru@gmail.com>
2023-12-12 16:11:48 +0000

    created Search separate component

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,36 +1,13 @@
 "use client";
 import "./Header.css";
 import Image from "../../../../node_modules/next/image";
-import { ChangeEvent } from "react";
-import { useRecoilState } from "recoil";
-import { searchState } from "@/app/recoil/atoms";
+import Search from "./components/Search/Search";
 
 const Header = () => {
-  const [search, setSearch] = useRecoilState(searchState);
-
-  const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
-    setSearch(e.target.value);
-  };
-  console.log(search);
   return (
     <div className="header-wrapper">
       <div className="header-main">
-        <form action="" className="search-bar">
-          <input
-            type="search"
-            required
-            value={search}
-            onChange={handleSearch}
-          />
-          <Image
-            src="/icons/search.svg"
-            alt="user icon"
-            width={20}
-            height={20}
-            className="search-icon"
-          />
-          <a href="javascript:void(0)" id="clear-btn"></a>
-        </form>
+        <Search />
         <div className="search-user">
           Hi, Gabriel
           <Image
---
+++ src/app/components/Header/components/Search/Search.tsx
@@ -0,0 +1,28 @@
+import "src/app/components/Header/Header.css";
+import { useRecoilState } from "recoil";
+import { searchState } from "@/app/recoil/atoms";
+import Image from "next/image";
+import { ChangeEvent } from "react";
+
+const Search = () => {
+  const [search, setSearch] = useRecoilState(searchState);
+
+  const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+  return (
+    <form action="" className="search-bar">
+      <input type="search" required value={search} onChange={handleSearch} />
+      <Image
+        src="/icons/search.svg"
+        alt="user icon"
+        width={20}
+        height={20}
+        className="search-icon"
+      />
+      <a href="javascript:void(0)" id="clear-btn"></a>
+    </form>
+  );
+};
+
+export default Search;
