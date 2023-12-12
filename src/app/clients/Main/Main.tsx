5465f30d175321103ed0f40fa85a0a98defff4bc
Lorena <lorenacapraru@gmail.com>
2023-12-12 19:03:00 +0000

    create Card component for clients

--- src/app/clients/Main/Main.tsx
+++ src/app/clients/Main/Main.tsx
@@ -1,7 +1,14 @@
+import Card from "./components/Card/Card";
 import "./Main.css";
 
 const Main = () => {
-  return <div>main clients</div>;
+  return (
+    <div>
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
 };
 
 export default Main;
