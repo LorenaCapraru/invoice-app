1d38dd6b67ddb915d9d710f0b140a6a910bc4ec3
Lorena <lorenacapraru@gmail.com>
2023-12-12 19:21:47 +0000

    created clients.json

--- src/app/clients/Main/Main.tsx
+++ src/app/clients/Main/Main.tsx
@@ -1,9 +1,15 @@
+"use client";
 import Card from "./components/Card/Card";
 import "./Main.css";
+import { useRecoilState } from "recoil";
+import { clientsState } from "@/app/recoil/atoms";
 
 const Main = () => {
+  const [client, setClient] = useRecoilState(clientsState);
+  console.log("lala", client.clients);
   return (
     <div>
+      {}
       <Card />
       <Card />
       <Card />
--- src/app/recoil/atoms.ts
+++ src/app/recoil/atoms.ts
@@ -1,3 +1,14 @@
 import { atom } from "recoil";
+import clients from "../clients.json";
+
+type Client = {
+  id: number;
+  name: string;
+  address: string;
+};
+export const clientsState = atom<Client[]>({
+  key: "clientsState",
+  default: clients.clients,
+});
 
 export const searchState = atom<string>({ key: "searchState", default: "" });
