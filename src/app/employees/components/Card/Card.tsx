a7e1e0c484df0a79528b86b3816039dd095172b3
Lorena <lorenacapraru@gmail.com>
2023-12-14 15:39:31 +0000

    cards style done

--- src/app/employees/components/Card/Card.tsx
+++ src/app/employees/components/Card/Card.tsx
@@ -13,21 +13,93 @@ type Employee = {
 };
 
 const Card = ({ el }: { el: Employee }) => {
-  // Generate a DiceBear Ring avatar URL based on the email address
-  // const avatarUrl = `https://avatars.dicebear.com/api/rings/${el.email}.svg`;
+  const handleCopyClick = () => {
+    const detailsToCopy = `Name: ${el.name}\nDOB: ${el.dob}\nEmail: ${el.email}\nUTR: ${el.UTR}\nNINO: ${el.NINO}\nAddress: ${el.address}\nPhone: ${el.phone_no}`;
 
+    // Use navigator.clipboard.writeText to copy details to the clipboard
+    navigator.clipboard.writeText(detailsToCopy).then(() => {
+      alert("Details copied to clipboard!");
+    });
+  };
   return (
     <div className="card-container">
       <div className="image-container">
+        <div className="copy">
+          <Image
+            src="/icons/copy.svg"
+            alt="copy"
+            width={25}
+            height={25}
+            onClick={handleCopyClick}
+          />
+        </div>
         <Image
           src="/icons/construction1.svg"
           alt="close navbar"
           width={90}
           height={90}
         />
+        <span className="card-name">{el.name}</span>
       </div>
-      <p>{el.name}</p>
-      {/* Add other details like date of birth, address, etc. here */}
+      <div className="employee-details">
+        <span>
+          <Image
+            src="/icons/dob.svg"
+            alt="close navbar"
+            width={20}
+            height={20}
+          />
+          <span className="db-data">{el.dob}</span>
+        </span>
+        <span>
+          <Image
+            src="/icons/email.svg"
+            alt="close navbar"
+            width={20}
+            height={20}
+          />
+          <span className="db-data">{el.email}</span>
+        </span>
+        <span>
+          <Image
+            src="/icons/nino.svg"
+            alt="close navbar"
+            width={20}
+            height={20}
+          />
+          <span className="db-data"> {el.UTR}</span>
+        </span>
+        <span>
+          <Image
+            src="/icons/nino.svg"
+            alt="close navbar"
+            width={20}
+            height={20}
+          />
+          <span className="db-data"> {el.NINO}</span>
+        </span>
+        <span>
+          <Image
+            src="/icons/address.svg"
+            alt="close navbar"
+            width={20}
+            height={20}
+          />
+          <span className="db-data">{el.address}</span>
+        </span>
+        <span>
+          <Image
+            src="/icons/phone.svg"
+            alt="close navbar"
+            width={20}
+            height={20}
+          />
+          <span className="db-data">{el.phone_no}</span>
+        </span>
+      </div>
+      {/* <button className="copy-button" onClick={handleCopyClick}>
+        Copy Details
+      </button> */}
     </div>
   );
 };
