351495dd9906645e415898d61077a70611e7e2b5
Lorena <lorenacapraru@gmail.com>
2023-12-18 18:28:08 +0000

    added company to recoil

--- src/app/recoil/atoms.ts
+++ src/app/recoil/atoms.ts
@@ -2,6 +2,8 @@ import { atom } from "recoil";
 import clients from "../clients.json";
 import sites from "../site.json";
 import employees from "../employees.json";
+import company from "../company.json";
+
 type Client = {
   id: number;
   name: string;
@@ -23,6 +25,18 @@ type Employee = {
   address: string;
   phone_no: string;
 };
+
+type Company = {
+  id: number;
+  name: string;
+  address: string;
+  postcode: string;
+  company_registration_number: string;
+  utr: number;
+  vat_reg_no: string;
+  sort_code: string;
+  account_number: number;
+};
 export const clientsState = atom<Client[]>({
   key: "clientsState",
   default: clients.clients,
@@ -34,5 +48,9 @@ export const siteState = atom<Site[]>({
 export const employeeState = atom<Employee[]>({
   key: "employeeState",
   default: employees.employees,
+});
+export const companyState = atom<Company>({
+  key: "companyState",
+  default: company.company[0],
 });
 export const searchState = atom<string>({ key: "searchState", default: "" });
