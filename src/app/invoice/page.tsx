026b9c34d6f41d41dcc08327ab15292855941c06
Lorena <lorenacapraru@gmail.com>
2023-12-27 18:00:25 +0000

    logic for export done

--- src/app/invoice/page.tsx
+++ src/app/invoice/page.tsx
@@ -9,8 +9,6 @@ import CompanyCard from "./components/CompanyCard/CompanyCard";
 import SelectClient from "./components/SelectClient/SelectClient";
 import InvoiceItems from "./components/InvoiceItems/InvoiceItems";
 import CompanyData from "./components/CompanyData/CompanyData";
-import html2pdf from "html2pdf.js";
-import html2canvas from "html2canvas";
 
 const Invoice = () => {
   const [startWeekClick, seStartWeekClick] = useState<boolean | null>(false);
@@ -36,30 +34,46 @@ const Invoice = () => {
     setClickExport(true);
   };
 
-  const generatePDF = async (): Promise<void> => {
-    const element: HTMLElement | null =
-      document.getElementById("pdfContentToExport");
-    if (element) {
-      const canvas: HTMLCanvasElement = await html2canvas(element);
-      const pdf: any = new html2pdf(element, {
-        margin: 10,
-        filename: "Invoice_CGM.pdf",
-        image: { type: "pdf", quality: 0.98 },
-        html2canvas: { scale: 2, logging: true },
-        jsPDF: { unit: "mm", format: "a4", orientation: "portrait" },
-      });
-      pdf.fromCanvas(canvas);
-    } else {
-      console.error("Element not found");
-    }
-  };
+  const [map, setMap] = useState(null);
 
   useEffect(() => {
-    if (process.browser && clickExport === true) {
+    const generatePDF = async (): Promise<void> => {
+      const element: HTMLElement | null =
+        document.getElementById("pdfContentToExport");
+      try {
+        if (element) {
+          const html2canvas = await import("html2canvas");
+          const html2pdf = await import("html2pdf.js");
+
+          const canvas: HTMLCanvasElement = await html2canvas.default(element);
+          console.log("Canvas generated:", canvas);
+
+          if (canvas) {
+            const pdf: any = new html2pdf.default(element, {
+              margin: 10,
+              filename: "Invoice_CGM.pdf",
+              image: { type: "pdf", quality: 0.98 },
+              html2canvas: { scale: 2, logging: true },
+              jsPDF: { unit: "mm", format: "a4", orientation: "portrait" },
+            });
+
+            pdf.fromCanvas(canvas);
+          } else {
+            console.error("Canvas not generated");
+          }
+        } else {
+          console.error("Element not found");
+        }
+      } catch (error) {
+        console.error("Error occurred while generating PDF:", error);
+      }
+    };
+
+    if (clickExport) {
       generatePDF();
+      setClickExport(false);
     }
-    setClickExport(false);
-  }, [clickExport, setClickExport]);
+  }, [clickExport]);
 
   return (
     <div className="invoice-main">
