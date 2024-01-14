135da3c06d2b9905e8d15ef12969d9d256107974
Lorena <lorenacapraru@gmail.com>
2024-01-14 16:57:05 +0000

    Search bar fixed styling for dark/light

--- src/app/components/Header/components/Search/Search.tsx
+++ src/app/components/Header/components/Search/Search.tsx
@@ -3,16 +3,26 @@ import { useRecoilState } from "recoil";
 import { searchState } from "@/app/recoilData/atoms";
 import Image from "next/image";
 import { ChangeEvent } from "react";
+import { isSliderClickedState } from "@/app/recoilData/atoms";
 
 const Search = () => {
   const [search, setSearch] = useRecoilState(searchState);
-
+  const [isSliderClicked, setIsSliderClicked] =
+    useRecoilState<boolean>(isSliderClickedState);
   const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
   };
   return (
-    <form action="" className="search-bar">
-      <input type="search" required value={search} onChange={handleSearch} />
+    <form
+      className={` ${isSliderClicked ? "search-bar-dark" : "search-bar-light"}`}
+    >
+      <input
+        type="search"
+        required
+        value={search}
+        onChange={handleSearch}
+        placeholder="Search an item"
+      />
       <Image
         src="/icons/search.svg"
         alt="user icon"
