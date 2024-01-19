17b496ee613219b0c5be13c67d47ee721e4e97c9
Lorena <lorenacapraru@gmail.com>
2024-01-19 13:57:10 +0000

    sign in user

--- src/app/firebase/auth.ts
+++ src/app/firebase/auth.ts
@@ -1,65 +1,14 @@
 import {
   getAuth,
-  createUserWithEmailAndPassword,
   Auth,
   UserCredential,
-  signInWithEmailAndPassword,
-  updateProfile,
   GoogleAuthProvider,
   signInWithPopup,
-  GithubAuthProvider,
   signOut,
 } from "firebase/auth";
-import { SignUpState, SingInState } from "../recoil/atoms";
 import { auth } from "./firebase";
 
 const authInstance: Auth = auth;
-
-// Sign Up
-export async function signUpUser(
-  authData: SignUpState,
-  fullName: string
-): Promise<UserCredential> {
-  const { email, password } = authData;
-
-  try {
-    const userCredential = await createUserWithEmailAndPassword(
-      authInstance,
-      email,
-      password
-    );
-    //to save the user's name
-    if (auth.currentUser) {
-      await updateProfile(auth.currentUser, {
-        displayName: fullName,
-      });
-    }
-
-    return userCredential;
-  } catch (error: any) {
-    console.error("Error signing up:", error.message);
-    throw error;
-  }
-}
-
-// Sign In
-export async function signInUser(
-  authData: SingInState
-): Promise<UserCredential> {
-  const { email, password } = authData;
-
-  try {
-    const userCredential = await signInWithEmailAndPassword(
-      authInstance,
-      email,
-      password
-    );
-    return userCredential;
-  } catch (error: any) {
-    console.error("Error signing up:", error.message);
-    throw error;
-  }
-}
 
 // Sign Up/In with Google
 export async function signUpWithGoogle(): Promise<UserCredential> {
@@ -74,11 +23,10 @@ export async function signUpWithGoogle(): Promise<UserCredential> {
 }
 
 //sign out
-export async function signOutUser(): Promise<boolean> {
+export async function signOutUser(): Promise<void> {
   const auth: Auth = getAuth();
   try {
     await signOut(auth);
-    return true;
   } catch (error: any) {
     console.error("Error signing out:", error.message);
     throw error;
